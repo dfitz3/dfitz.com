4a5c9c58e9db43097f4401a834c6d8318e592cd0
dfitz <d@dfitz.com>
2013-10-11 06:31:40 +0000

    Header Updates. -Implemented font size selector. -Cleaned up bloat. -Adjusted search styling and form

--- header.php
+++ header.php
@@ -1,3 +1,13 @@
+<?php
+
+$fontSize = 12;
+$fontSizeCookieName = 'fontSize';
+if(isset($_COOKIE[$fontSizeCookieName])) {
+	$fontSize = intval($_COOKIE[$fontSizeCookieName]); 
+}
+
+
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -25,6 +35,41 @@
 		 <script src="../../assets/js/html5shiv.js"></script>
 		 <script src="../../assets/js/respond.min.js"></script>
 		 <![endif]-->
+		<script>
+			(function($){
+				$(document).ready(function(){
+					$("span[id*='font-size']").click(function(d) {
+						document.cookie = 'fontSize=' + $(this).attr("data-font-size");	
+						window.location.reload();
+					});
+				});
+			})(jQuery);
+		</script>
+		<style>
+			body {
+				font-size: <?=$fontSize?>px;
+			}
+			.font-size { 
+				margin: 5px;
+				line-height: 40px;
+				height: 40px;
+				width: 40px;
+				display: inline-block;
+				border: 1px solid #CDCDCD;
+				background: #FFF;
+				text-align: center;
+				float: left;
+				margin-left: 15px;
+			}
+			#font-size1 { font-size: 14px; }
+			#font-size2 { font-size: 18px; }
+			#font-size3 { font-size: 28px; }
+
+			/* overrides */
+			.navbar[role='navigation'] { margin-top: 5px; border-radius: 5px; }
+			.navbar-default { background-color: #FFF; }
+			header #search-form { width: 260px; margin-top: 10px; }
+		</style>
 	 </head>
 	 <body>
 		 <header>
@@ -38,25 +83,26 @@
 					 <span class="icon-bar"></span>
 					 <span class="icon-bar"></span>
 				 </button>
-				 <a class="navbar-brand" href="#">DFitz</a>
+				 <a class="navbar-brand" href="/">DFitz</a>
 			 </div>
 
 			 <!-- Collect the nav links, forms, and other content for toggling -->
 			 <div class="collapse navbar-collapse navbar-ex1-collapse">
-				 <ul class="nav navbar-nav">
-					 <li class="active"><a href="#">1</a></li>
-				 </ul>
-				 <form class="navbar-form navbar-left" role="search">
-					 <div style='max-width:300px;' class="input-group">
-						 <input id='search-term' type="text" class="form-control" style='text-align:center;' placeholder="^_^">
+				 <form action='/search/' method='get' class="navbar-left" id='search-form' role="search">
+					 <div style='' class="input-group">
+						 <input id='search-term' name='q' type="text" class="form-control" style='text-align:center;' placeholder="^_^">
 						 <span class='input-group-btn'>
 							 <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
 						 </span> 
 					 </div>
 				 </form>
 				 <ul class="nav navbar-nav navbar-right">
-					 <li><a href="#">2</a></li>
-					 <li><a href="#">3</a></li>
+					 <li><a href="#projects">projects</a></li>
+					 <li id='font-selector-wrapper'>
+						<span id='font-size1' data-font-size='14' class='font-size'>A</span>
+						<span id='font-size2' data-font-size='18' class='font-size'>A</span>
+						<span id='font-size3' data-font-size='28' class='font-size'>A</span>	
+					</li>
 				 </ul>
 			 </div><!-- /.navbar-collapse -->
 			 </nav>
